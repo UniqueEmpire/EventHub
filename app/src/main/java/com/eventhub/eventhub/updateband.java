4b77e80412d37c8fbf124d4f49acd4427422cb37
uabinesh <79247690+uabinesh@users.noreply.github.com>
2021-10-03 04:14:46 +0530

    Update form

---
+++ app/src/main/java/com/eventhub/eventhub/updateband.java
@@ -0,0 +1,14 @@
+package com.eventhub.eventhub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class updateband extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_updateband);
+    }
+}
